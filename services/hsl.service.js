2db2884c361a41a2988e570ba7ba3a6bb0f9fe50
Pinja Kuosmanen <pmkuosmanen@gmail.com>
2018-10-13 14:10:34 +0300

    eventsource handlers encapsulated

--- services/hsl.service.js
+++ services/hsl.service.js
@@ -1,16 +1,26 @@
 var evtSource = new EventSource ('')
 
-evtSource.onopen = e => {
-    console.log('connected!')
-    console.log(e)
+const registerEventListener = (handler) => {
+
+    evtSource.onopen = e => {
+        console.log('connected!')
+        console.log(e)
+    };
+    
+    evtSource.onmessage = e => {
+        console.log ('got message!')
+        console.log(e)
+        return handler(e)
+    };
+    
+    evtSource.onerror = e => {
+        console.log('got error!')
+        console.log(e)
+    };
 };
 
-evtSource.onmessage = e => {
-    console.log ('got message!')
-    console.log(e)
+const unregisterEventListener = () => {
+    evtSource.close()
 };
 
-evtSource.onerror = e => {
-    console.log('got error!')
-    console.log(e)
-}
+export { registerEventListener, unregisterEventListener }
